c4f4164f3b3c654204e060a951e03e03836a5230
LucaReynaldo <luca.reynaldo@mi.unc.edu.ar>
2024-09-28 22:30:14 -0300

    implementacion final con sus respectivos test

--- lista/src/App.jsx
+++ lista/src/App.jsx
@@ -1,31 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { GenericList } from "./GenericList";
 
 // Ejemplo de render para GenericList
 const renderItem = (item) => (
-  <div>
-    <div key={item.id}>
-      <h1>{item.id + ") Title: " + item.title}</h1>
-      <h2>{"UserId: " + item.userId}</h2>
-      <p>{item.completed.toString()}</p>
-      <button>Click Me</button>
-    </div>
+  <div key={item.unique_id} style={{ padding: 10, margin: 10, backgroundColor: '#00061a', borderRadius: '30px' }}>
+      <h1>{item.name}</h1>
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px' }}>
+          <div>
+              <h2>{"Cantidad de Jugadores: " + item.players.length + "/4"}</h2>
+              <p>{"Estado: " + item.state}</p>
+              <p>{"Dueño: " + item.creator}</p>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <button style={{backgroundColor: '#001866' }}>
+                UNIRSE</button>
+          </div>
+      </div>
   </div>
 );
 
-function App() {
 
+function App() {
+  const [search, setSearch] = useState("");
+  const [numGames, setNumGames] = useState(3);
+  
   return (
-    <div>
-      <h1>Aplicación WebSocket</h1>
+    <div >
+      <h1>Test del Componente Lita Generica</h1>
+      <input placeholder="Ingresa un Nombre" onChange={(e) => {setSearch(e.target.value); console.log(search)}} />
+      <button>Buscar</button>
+      <div>
       <GenericList
-        apiEndpoint="https://jsonplaceholder.typicode.com/todos"
-        from={0}
-        to={10}
-        websocketUrl="ws://localhost:3000/"
-        renderItem={renderItem}
-      />
+          websocketUrl={"/apiWS/games"}
+          renderItem={renderItem}
+          typeKey={"CreatedGames"}
+        />
+      </div>
     </div>
   );
 }
