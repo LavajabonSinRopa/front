f6750a3f97d22cd88ae4eb4933663a417e8a5023
Martina Ibañez Wulbrandt <martinaibanezw@mi.unc.edu.ar>
2024-10-10 20:32:28 -0300

    SCRUM-39 add PlayerList tests, update file names

---
+++ src/tests/TestGameLobby/GameLobby.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import GameLobby from "../../containers/GameLobbyContainer/components/GameLobby.jsx";
+import { UserIdContext } from "../../contexts/UserIdContext.jsx";
+
+const mockGameData = {
+	gameName: "Test Game",
+	gameId: "12345",
+	gameState: "waiting",
+	gameCreator: "player",
+};
+const mockPlayerList = [
+	["1", "player1"],
+	["2", "player2"],
+	["3", "player3"],
+];
+const mockPlayerId = "1";
+
+describe("GameLobby", () => {
+	beforeEach(() => {
+		// Limpia los mocks antes de cada prueba
+		jest.clearAllMocks();
+	});
+
+	it("Se renderizan los componentes internos correctamente", () => {
+		render(
+				<GameLobby
+					gameData={mockGameData}
+					playerList={mockPlayerList.map((player) => player[1])}
+					playerId={mockPlayerId}
+				/>
+		);
+
+		expect(screen.getByText("Test Game")).toBeInTheDocument();
+		expect(screen.getByText("Pública")).toBeInTheDocument();
+		expect(screen.getByText("player1")).toBeInTheDocument();
+		expect(screen.getByText("player2")).toBeInTheDocument();
+		expect(screen.getByText("player3")).toBeInTheDocument();
+		expect(screen.getByText("Iniciar Partida")).toBeInTheDocument();
+		expect(screen.getByText("Cancelar Partida")).toBeInTheDocument();
+		expect(screen.getByText("Abandonar Partida")).toBeInTheDocument();
+	});
+});
