ce36b849a73f4c4b62bfdca75310f7c005422e93
LucaReynaldo <luca.reynaldo@mi.unc.edu.ar>
2024-10-12 02:25:17 -0300

    Optimizacion dewebsocket en gamelobbycontainer terminada y pasa los tests

--- src/containers/GameLobbyContainer/GameLobbyContainer.jsx
+++ src/containers/GameLobbyContainer/GameLobbyContainer.jsx
@@ -4,33 +4,40 @@ import GameLobby from "./components/GameLobby";
 import { UserIdContext } from "../../contexts/UserIdContext";
 
 export const GameLobbyContainer = () => {
-  //obtiene game_id de la url
   const { game_id } = useParams();
-  //contexto global que tiene la userId de la ultima partida a la que entro el usuario
-  const { userId, setUserId } = useContext(UserIdContext);
+  const { userId } = useContext(UserIdContext);
   const [gameData, setGameData] = useState(null);
   const [playerList, setPlayerList] = useState([]);
+  const [reconnectingAPI, setReconnectingAPI] = useState(false); // Estado para reconexion para la API
+  const [reconnectingWS, setReconnectingWS] = useState(false); // Estado para reconexion para WS
   const socketRef = useRef(null);
+  const reconnectTimeoutRefWS = useRef(null);
+  const reconnectTimeoutRefAPI = useRef(null);
+  const reconnectInterval = 5000; // Intervalo de reconexion de 5 segundos
+  const isMounted = useRef(true); // Para verificar si el componente sigue montado
 
-  useEffect(() => {
-    //La logica que hace fetch al principio
-    const fetchGameData = async () => {
-      if (game_id) {
-        try {
-          const response = await fetch(`/api/games/${game_id}`, {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
+  // Fetch inicial de los datos del juego
+  const fetchGameData = async () => {
+    if (game_id) {
+      try {
+        const response = await fetch(`/api/games/${game_id}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-          if (!response.ok) {
-            console.log(
-              "Hubo un problema al crear la partida, intenta de nuevo."
-            );
-            return;
-          }
-
+        if (!response.ok) {
+          console.log(
+            "Hubo un problema al crear la partida, intentando de nuevo..."
+          );
+          setReconnectingAPI(true); // Iniciar estado de reconexion
+          // Reintentar despues del intervalo
+          reconnectTimeoutRefAPI.current = setTimeout(() => {
+            fetchGameData(); // Volver a intentar el fetch
+          }, reconnectInterval);
+          return;
+        } else {
           const result = await response.json();
           const game = result;
 
@@ -42,62 +49,109 @@ export const GameLobbyContainer = () => {
           });
 
           const { player_names, players } = game;
-          const tuples = player_names.map((name, index) => [players[index], name]);
+          const tuples = player_names.map((name, index) => [
+            players[index],
+            name,
+          ]);
           setPlayerList(tuples);
-        } catch (error) {
-          console.error("Error:", error);
-        } finally {
-          console.log("finaly");
+          setReconnectingAPI(false); // Si el fetch es exitoso, cancelar el estado de reconexion
+          if (reconnectTimeoutRefAPI.current)
+            clearTimeout(reconnectTimeoutRefAPI.current);
         }
+      } catch (error) {
+        console.error("Error:", error);
+        setReconnectingAPI(true); // Activar reconexion en caso de error
+        reconnectTimeoutRefAPI.current = setTimeout(() => {
+          fetchGameData(); // Reintentar el fetch después del intervalo
+        }, reconnectInterval);
+      } finally {
+        console.log("finally");
+      }
+    }
+  };
+
+  // useEffect para iniciar el fetch cuando el componente se monte
+  useEffect(() => {
+    fetchGameData();
+
+    // Limpiar el timeout de reconexion cuando el componente se desmonte
+    return () => {
+      if (reconnectTimeoutRefAPI.current)
+        clearTimeout(reconnectTimeoutRefAPI.current);
+    };
+  }, [game_id]);
+
+  // Funcion para conectar al WebSocket
+  const connectWebSocket = () => {
+    if (!game_id || !userId) return;
+
+    socketRef.current = new WebSocket(`/apiWS/games/${game_id}/${userId}`);
+    socketRef.current.onopen = () => {
+      console.log("WebSocket connected");
+      setReconnectingWS(false); // Conexion exitosa, cancelar el estado de reconexion
+      if (reconnectTimeoutRefWS.current)  
+        clearTimeout(reconnectTimeoutRefWS.current);
+    };
+
+    socketRef.current.onclose = () => {
+      console.log("WebSocket closed, attempting to reconnect...");
+      setReconnectingWS(true); // Establecer el estado de reconexion
+      // Intentar reconectar solo si el componente sigue montado
+      if (isMounted.current) {
+        reconnectTimeoutRefWS.current = setTimeout(() => {
+          connectWebSocket();
+        }, reconnectInterval);
       }
     };
 
-    fetchGameData();
-  }, [game_id]);
-
-
-
-  // Inicializacion y Cierre del WebSocket
-  useEffect(() => {
-    if (!game_id || !userId) {
-      return;
-    }
-
-    socketRef.current = new WebSocket(`/apiWS/games/${game_id}/${userId}`);
-    socketRef.current.onopen = () => console.log("ws opened");
-    socketRef.current.onclose = () => console.log("ws closed");
     socketRef.current.onerror = (event) => {
       console.error("WebSocket error observed:", event);
     };
-    const wsCurrent = socketRef.current;
-
-    return () => {
-      wsCurrent.close();
-    };
-  }, []);
-
-  //Aca se maneja la comunicacion por websocket
-  useEffect(() => {
-    if (!socketRef.current) return;
 
     socketRef.current.onmessage = (event) => {
       const message = JSON.parse(event.data);
       console.log(message);
       if (message.type === "PlayerJoined") {
         const { player_id, player_name } = message.payload;
-        setPlayerList(prevPlayers => [...prevPlayers, [player_id, player_name]]);
+        setPlayerList((prevPlayers) => [
+          ...prevPlayers,
+          [player_id, player_name],
+        ]);
       }
+    };
+  };
+
+  // Inicializacion y cierre del WebSocket
+  useEffect(() => {
+    isMounted.current = true; // Marcar el componente como montado
+    // Solo conectar si estamos montados y tenemos valores validos
+    if (game_id && userId) {
+      // Solo conecta si no hay conexion activa
+      if (!socketRef.current || socketRef.current.readyState === WebSocket.CLOSED) {
+        connectWebSocket();
+      }
+    }
+    // Limpiar el WebSocket y el timeout de reconexion cuando el componente se desmonte
+    return () => {
+      isMounted.current = false; // Marcar el componente como desmontado
+      if (socketRef.current) socketRef.current.close();
+      if (reconnectTimeoutRefWS.current)
+        clearTimeout(reconnectTimeoutRefWS.current);
     };
   }, [game_id, userId]);
 
   return (
     <div>
       {gameData ? (
-        <GameLobby
-          gameData={gameData}
-          playerList={playerList}
-          playerId={userId}
-        />
+        (reconnectingAPI || reconnectingWS) ? (
+          <p>Reconectando...</p>
+        ) : (
+          <GameLobby
+            gameData={gameData}
+            playerList={playerList}
+            playerId={userId}
+          />
+        )
       ) : (
         <p>Loading...</p>
       )}
