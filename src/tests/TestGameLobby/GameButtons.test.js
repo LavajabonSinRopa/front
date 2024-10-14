5e7c1a20cfdcb60cb2ec759bc9448da6c7583c68
ivo-pfaffen <ivopfaffen@gmail.com>
2024-10-14 19:02:30 -0300

    SCRUM-16 fix GameButtons tests

--- src/tests/TestGameLobby/GameButtons.test.js
+++ src/tests/TestGameLobby/GameButtons.test.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import GameButtons from "../../containers/GameLobbyContainer/components/GameButtons/GameButtons.jsx"
+import GameButtons from "../../containers/GameLobbyContainer/components/GameButtons/GameButtons.jsx";
 
 describe("GameButtons", () => {
 	const mockOnStartGame = jest.fn();
-	//const mockOnCancelGame = jest.fn();
 	const mockOnLeaveGame = jest.fn();
 
 	it("Muestra el botón de 'Iniciar Partida' solo para el owner", () => {
@@ -14,14 +13,12 @@ describe("GameButtons", () => {
 				playerId="ownerId"
 				ownerId="ownerId"
 				onStartGame={mockOnStartGame}
-				//onCancelGame={mockOnCancelGame}
 				onLeaveGame={mockOnLeaveGame}
+				playerList={["ownerId", "playerId1", "playerId2", "playerId3"]} // Mock player list with 4 players
 			/>
 		);
 
 		expect(screen.getByText("Iniciar Partida")).toBeInTheDocument();
-
-		//expect(screen.getByText("Cancelar Partida")).toBeInTheDocument();
 	});
 
 	it("No muestra el botón de 'Iniciar Partida' para un jugador que no es el owner", () => {
@@ -30,13 +27,12 @@ describe("GameButtons", () => {
 				playerId="playerId"
 				ownerId="ownerId"
 				onStartGame={mockOnStartGame}
-				//onCancelGame={mockOnCancelGame}
 				onLeaveGame={mockOnLeaveGame}
+				playerList={["ownerId", "playerId1", "playerId2", "playerId3"]} // Mock player list with 4 players
 			/>
 		);
 
 		expect(screen.queryByText("Iniciar Partida")).not.toBeInTheDocument();
-
 		expect(screen.getByText("Abandonar Partida")).toBeInTheDocument();
 	});
 
@@ -46,13 +42,12 @@ describe("GameButtons", () => {
 				playerId="ownerId"
 				ownerId="ownerId"
 				onStartGame={mockOnStartGame}
-				//onCancelGame={mockOnCancelGame}
 				onLeaveGame={mockOnLeaveGame}
+				playerList={["ownerId", "playerId1", "playerId2", "playerId3"]} // Mock player list with 4 players
 			/>
 		);
 
 		fireEvent.click(screen.getByText("Iniciar Partida"));
-
 		expect(mockOnStartGame).toHaveBeenCalledTimes(1);
 	});
 
@@ -78,13 +73,12 @@ describe("GameButtons", () => {
 				playerId="playerId"
 				ownerId="ownerId"
 				onStartGame={mockOnStartGame}
-				//onCancelGame={mockOnCancelGame}
 				onLeaveGame={mockOnLeaveGame}
+				playerList={["ownerId", "playerId1", "playerId2", "playerId3"]} // Mock player list with 4 players
 			/>
 		);
 
 		fireEvent.click(screen.getByText("Abandonar Partida"));
-
 		expect(mockOnLeaveGame).toHaveBeenCalledTimes(1);
 	});
 });
