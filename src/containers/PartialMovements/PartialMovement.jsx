b18e13f0e2a800b7dcd07d7856cde301a714d653
Martina Ibañez Wulbrandt <martinaibanezw@mi.unc.edu.ar>
2024-10-16 23:42:51 -0300

    SCRUM-45 add PartialMovement component (not finished)

---
+++ src/containers/PartialMovements/PartialMovement.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function PartialMovement() {
+  return <div className="PM">Partial Movement</div>;
+}
+export default PartialMovement;
--- src/containers/StartGame.jsx
+++ src/containers/StartGame.jsx
@@ -3,17 +3,21 @@ import { useParams } from "react-router-dom";
 import Board from "./Board/Board.jsx";
 import LeaveGame from "./LeaveGame/LeaveGame.jsx";
 import { UserIdContext } from "../contexts/UserIdContext.jsx";
-
+import PartialMovement from "./PartialMovements/PartialMovement.jsx";
+import "./StartGame.css";
 
 function StartGame() {
-	const { game_id } = useParams(); 
-    const { userId } = useContext(UserIdContext); 
+  const { game_id } = useParams();
+  const { userId } = useContext(UserIdContext);
 
-	return (
-		<div>
-			<Board />
-			<LeaveGame playerId={userId} gameId={game_id} />
-		</div>
-	);
+  return (
+    <div className="game-container">
+      <div className="board-container">
+        <Board />
+        <LeaveGame playerId={userId} gameId={game_id} />
+      </div>
+      <PartialMovement />
+    </div>
+  );
 }
 export default StartGame;
