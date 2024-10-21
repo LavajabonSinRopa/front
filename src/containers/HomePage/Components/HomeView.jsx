e0e89a2061085150a82b12e69c9840e00648d945
LucaReynaldo <luca.reynaldo@mi.unc.edu.ar>
2024-10-21 20:30:52 -0300

    Disenio de homepage

--- src/containers/HomePage/Components/HomeView.jsx
+++ src/containers/HomePage/Components/HomeView.jsx
@@ -1,34 +1,44 @@
 // HomeView.js
 import React from "react";
+import "./HomeView.css";
+import switcher_UI_UX_desing_banner from "./switcher_UI_UX_desing_banner.svg";
+import switcherAnimado from "./SwitcherAnimado.svg";
+import backgroundPolygon1 from "./backgroundPolygon1.svg";
+import shadow from "./shadow.svg"
 
 function HomeView({ onNavigate }) {
   return (
-    <div style={styles.container}>
-      <h1>SWITCHER</h1>
-      <button style={styles.button} onClick={() => onNavigate("/creategame")}>
-        Crear Partida
-      </button>
-      <button style={styles.button} onClick={() => onNavigate("/searchgame")}>
-        Buscar Partida
-      </button>
-    </div>
+    <>
+      <div className="homeContainer">
+      <img className={"backgroundPolygon1"} src={backgroundPolygon1} />
+      <img className={"shadow"} src={shadow} />
+        <div className="frame"></div>
+        <div className="homeColumn1">
+          <img
+            className={"switcher_UI_UX_desing_banner"}
+            src={switcher_UI_UX_desing_banner}
+          />
+          <button
+            className="homeButton"
+            onClick={() => onNavigate("/creategame")}
+          >
+            Crear Partida
+          </button>
+          <button
+            className="homeButton"
+            onClick={() => onNavigate("/searchgame")}
+          >
+            Buscar Partida
+          </button>
+        </div>
+        <div className="homeColumn2">
+          <img className={"switcherAnimado"} src={switcherAnimado} />
+        </div>
+      </div>
+      
+      <div className="homeBackground"></div>
+    </>
   );
 }
-
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    gap: "20px",
-  },
-  button: {
-    padding: "15px 30px",
-    fontSize: "16px",
-    cursor: "pointer",
-  },
-};
 
 export default HomeView;
