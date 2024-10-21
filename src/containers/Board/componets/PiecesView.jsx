8491e00ec9d72776a36ef985acac92796fa91585
Martina Ibañez Wulbrandt <martinaibanezw@mi.unc.edu.ar>
2024-10-21 19:28:39 -0300

    SCRUM-45 interpretacion de colores para movimientos parciales

--- src/containers/Board/componets/PiecesView.jsx
+++ src/containers/Board/componets/PiecesView.jsx
@@ -8,16 +8,20 @@ const PiecesView = ({
   movError,
   isSwapped,
 }) => {
-  const isAFormedFigure = color === color.toUpperCase();
-  const normalColor = color.toLowerCase();
+  const isInvolvedInPartialMovement = color.endsWith("%");
+  if (isInvolvedInPartialMovement) {
+    color = color.slice(0, -1);
+  }
+  const isAFormedFigure = color === color.toUpperCase(); // Si el color viene todo en mayusculas, es una figura formada y se resalta en el tablero
+  const normalColor = color.toLowerCase(); //ficha común, no se resalta
   const buttonClass = `button 
   ${moveableSlot ? "moveableSlot" : ""} 
   ${normalColor ? normalColor : ""} 
   ${movError ? "movError" : ""} 
   ${isSelected ? "isSelected" : ""} 
-  ${isSwapped ? "isSwapped" : ""} 
+  ${isSwapped  ? "isSwapped" : ""} 
   ${isAFormedFigure ? "isAFormedFigure" : ""} 
-  ${isSwapped && isAFormedFigure ? "isSwapped isAFormedFigure" : ""}`;
+  ${isSwapped && isAFormedFigure ? "isSwapped" : ""}`;
 
   return <button className={buttonClass}></button>;
 };
